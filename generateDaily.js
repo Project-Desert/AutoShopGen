a081a48888b2a1dc02c07a12076b3f8450aa6fcb
Sgt <141946332+SgtSegment@users.noreply.github.com>
2024-06-02 19:09:06 -0400

    Add files via upload

---
+++ generateDaily.js
@@ -0,0 +1,35 @@
+// this legit just generates outfits lmaooooo
+// also this fails A LOT
+const fs = require("node:fs");
+const { json } = require('stream/consumers');
+
+let amountGrabbed = 0;
+let grabbedIDS = [];
+grabNonSkin();
+
+function grabNonSkin() {
+   // const randomNum = Math.floor(Math.random() * 2026);
+    const randomType = Math.floor(Math.random() * 7);
+    const typesOfC = ["emotes", "backpacks", "pickaxes", "gliders", "wraps", "music", "emojis"];
+    const parsed = JSON.parse(fs.readFileSync('./dumpedCosmetics/'+typesOfC[randomType]+".json"));
+    const randomNum = Math.floor(Math.random() * parsed.data.length);
+    const randomOutfit = parsed.data[randomNum];
+
+    
+    // console.log(parsed.data.length+"\n"+randomOutfit);
+    if (randomOutfit.introduction.backendValue > 15) {
+        console.log("No bueno");
+        grabNonSkin(parsed);
+    } else {
+        console.log("Muy bueno: "+randomOutfit.id);
+        grabbedIDS.push(randomOutfit.id.toString()+"       "+typesOfC[randomType]);
+        amountGrabbed++;
+        if (amountGrabbed < 6) {
+            grabNonSkin(parsed);
+        } else {
+            // console.log("\n\nGrabbed CIDS:\n"+grabbedCIDS[0]+"\n"+grabbedCIDS[1]);
+            console.log("Dumping IDS into 'daily.txt'");
+            fs.writeFileSync("./daily.txt", grabbedIDS[0]+"\n"+grabbedIDS[1]+"\n"+grabbedIDS[2]+"\n"+grabbedIDS[3]+"\n"+grabbedIDS[4]+"\n"+grabbedIDS[5]);
+        }
+    }
+}
---
+++ generateFeatured.js
@@ -0,0 +1,31 @@
+// this legit just generates outfits lmaooooo
+// also this fails A LOT
+var XMLHttpRequest = require('xhr2');
+const fs = require("node:fs");
+const { json } = require('stream/consumers');
+
+let amountGrabbed = 0;
+let grabbedCIDS = [];
+let parsedOutfits = JSON.parse(fs.readFileSync('./dumpedCosmetics/outfits.json'));
+// console.log(parsedOutfits.data[0]);
+grabSkin(parsedOutfits);
+
+function grabSkin(parsed) {
+    const randomNum = Math.floor(Math.random() * 2026);
+    const randomOutfit = parsed.data[randomNum];
+    // console.log(parsed.data.length+"\n"+randomOutfit);
+    if (randomOutfit.introduction.backendValue > 15) {
+        console.log("No bueno");
+        grabSkin(parsed);
+    } else {
+        console.log("Muy bueno: "+randomOutfit.id);
+        grabbedCIDS.push(randomOutfit.id.toString());
+        amountGrabbed++;
+        if (amountGrabbed < 2) {
+            grabSkin(parsed);
+        } else {
+            console.log("\n\nGrabbed CIDS:\n"+grabbedCIDS[0]+"\n"+grabbedCIDS[1]+"\n\nDumping into featured.txt");
+            fs.writeFileSync('./featured.txt', grabbedCIDS[0]+"\n"+grabbedCIDS[1]);
+        }
+    }
+}
